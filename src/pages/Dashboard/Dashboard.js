dd4bc7d52f20a428bc33837c8a87af00fe74ef37
prajwal4518 <prajwalpadal17@gmail.com>
2021-02-27 13:33:25 +0530

    Co-authored-by: sumitkumar105 <sumitkumar105@users.noreply.github.com> Co-authored-by: Rupayan Karanjai <Rupayank@users.noreply.github.com> Co-authored-by: Rohit Deshpande <RohitDeshpande49@users.noreply.github.com>

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,35 +1,63 @@
 import React from "react";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
+import { FaRupeeSign } from "react-icons/fa";
 import "./Dashboard.css";
 
 export default function Dashboard() {
   return (
-    <>
-      <div className="MainDashboardContainer">
-        <div className="HeaderRow_1">
-          <div className="PageHeader">
-            <span>Dashboard</span>
+    <div className="MainDashboardContainer">
+      <div className="HeaderRow_1">
+        <div className="PageHeader">
+          <span> Dashboard </span>
+        </div>
+        <div className="DBSearchBar">
+          <input type="text" placeholder="Search here..."></input>
+        </div>
+        <div className="ProfileContainer">
+          <div className="noticationIcon">
+            <IoMdNotificationsOutline> </IoMdNotificationsOutline>
           </div>
-          <div className="DBSearchBar">
-            <input type="text" placeholder="Search here..."></input>
+          <div classNmae="ProfileContainer" />
+          <div className="ProfileIconContainer">
+            <CgProfile> </CgProfile>
           </div>
-          <div className="ProfileContainer">
-            <div className="noticationIcon">
-              <IoMdNotificationsOutline></IoMdNotificationsOutline>
-            </div>
-            <div classNmae="ProfileContainer" />
-            <div className="ProfileIconContainer">
-              <CgProfile></CgProfile>
-            </div>
-            <div className="profileName">
-              <span>SumitKumar Deshpande</span>
-            </div>
+          <div className="profileName">
+            <span> SumitKumar Deshpande </span>
           </div>
         </div>
-        <div className="QuickReviewRow_2"></div>
-        <div className="MainGraphsRow_3"></div>
       </div>
-    </>
+      <div className="QuickReviewRow_2">
+        <div className="QRContainer">
+          <div className="HeaderContainer">
+            <div className="QRHeading">Rs 20 Lack</div>
+            <div className="QRSubHeading">Total Expenses</div>
+          </div>
+          <div className="QRtrailingIcon">
+            <FaRupeeSign />
+          </div>
+        </div>
+        <div className="QRContainer">
+          <div className="HeaderContainer">
+            <div className="QRHeading">Rs 30 Lack</div>
+            <div className="QRSubHeading">Total Profit</div>
+          </div>
+          <div className="QRtrailingIcon">
+            <FaRupeeSign />
+          </div>
+        </div>
+        <div className="QRContainer">
+          <div className="HeaderContainer">
+            <div className="QRHeading">Rs 10 Lack</div>
+            <div className="QRSubHeading">Total Revenue</div>
+          </div>
+          <div className="QRtrailingIcon">
+            <FaRupeeSign />
+          </div>
+        </div>
+      </div>
+
+      <div className="MainGraphsRow_3"> </div>
+    </div>
   );
 }
