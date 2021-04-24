093f291231315a55c54a712d2cd4faf8b9743ea8
Rupayan Karanjai <59760234+Rupayank@users.noreply.github.com>
2021-04-25 01:35:50 +0530

    Minor routing & button fixes
    Added temporary complete button, fixed  form back button route in Loom process, added required process checkbox in order form

--- src/Worker/Worker/Repairing.js
+++ src/Worker/Worker/Repairing.js
@@ -5,109 +5,110 @@ import "./../ErrorMessages/Error.css";
 import "./InputFormStyle/formBGStyle.css";
 
 function Repairing() {
-  const [form, setForm] = useState({
-    order_no: "",
-    date: "",
-    machine: "",
-    worker: "",
-  });
+	const [form, setForm] = useState({
+		order_no: "",
+		date: "",
+		machine: "",
+		worker: "",
+	});
 
-  function handleSubmit(e) {
-    e.preventDefault();
+	function handleSubmit(e) {
+		e.preventDefault();
 
-    axios
-      .post("http://localhost:3006/repairing", form)
-      .then((res) => {
-        console.log(res);
-        alert("successful insert");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+		axios
+			.post("http://localhost:3006/repairing", form)
+			.then((res) => {
+				console.log(res);
+				alert("successful insert");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 
-    setForm({
-      order_no: "",
-      date: "",
-      machine: "",
-      worker: "",
-    });
-  }
+		setForm({
+			order_no: "",
+			date: "",
+			machine: "",
+			worker: "",
+		});
+	}
 
-  function handleChange(event) {
-    const { name, value } = event.target;
+	function handleChange(event) {
+		const { name, value } = event.target;
 
-    setForm((prv) => {
-      return {
-        ...prv,
-        [name]: value,
-      };
-    });
-  }
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div class="login">
-          <div class="form">
-            <div className="upperButtons">
-              <NavLink to="/worker1">
-                <button class="button">Back</button>
-              </NavLink>
-              <a class="button" href="#popup1">
-                Error
-              </a>
-            </div>
-            <h2>Repairing Form</h2>
+		setForm((prv) => {
+			return {
+				...prv,
+				[name]: value,
+			};
+		});
+	}
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div class="login">
+					<div class="form">
+						<div className="upperButtons">
+							<NavLink to="/worker1">
+								<button class="button">Back</button>
+							</NavLink>
+							<button class="button">Complete</button>
+							<a class="button" href="#popup1">
+								Error
+							</a>
+						</div>
+						<h2>Repairing Form</h2>
 
-            <input
-              type="number"
-              value={form.order_no}
-              onChange={handleChange}
-              name="order_no"
-              placeholder="Order Number"
-              required
-            />
-            <input
-              type="date"
-              value={form.date}
-              onChange={handleChange}
-              name="date"
-              placeholder="Date"
-              required
-            />
-            <input
-              type="number"
-              value={form.machine}
-              onChange={handleChange}
-              name="machine"
-              placeholder="Repairing Machine"
-              required
-            />
-            <input
-              type="text"
-              value={form.worker}
-              onChange={handleChange}
-              name="worker"
-              placeholder="Worker"
-              required
-            />
+						<input
+							type="number"
+							value={form.order_no}
+							onChange={handleChange}
+							name="order_no"
+							placeholder="Order Number"
+							required
+						/>
+						<input
+							type="date"
+							value={form.date}
+							onChange={handleChange}
+							name="date"
+							placeholder="Date"
+							required
+						/>
+						<input
+							type="number"
+							value={form.machine}
+							onChange={handleChange}
+							name="machine"
+							placeholder="Repairing Machine"
+							required
+						/>
+						<input
+							type="text"
+							value={form.worker}
+							onChange={handleChange}
+							name="worker"
+							placeholder="Worker"
+							required
+						/>
 
-            <input type="submit" value="Sign In" class="submit" />
-          </div>
-        </div>
-      </form>
-      <div id="popup1" class="overlay">
-        <div class="popup">
-          <h2>Report Error</h2>
-          <a class="close" href="#">
-            &times;
-          </a>
-          <div class="content">
-            <input type="text" placeholder="error" />
-            <button className="MainButton">Submit</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<input type="submit" value="Sign In" class="submit" />
+					</div>
+				</div>
+			</form>
+			<div id="popup1" class="overlay">
+				<div class="popup">
+					<h2>Report Error</h2>
+					<a class="close" href="#">
+						&times;
+					</a>
+					<div class="content">
+						<input type="text" placeholder="error" />
+						<button className="MainButton">Submit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Repairing;
--- src/Worker/Worker/Shift.js
+++ src/Worker/Worker/Shift.js
@@ -6,82 +6,83 @@ import "./InputFormStyle/formBGStyle.css";
 import "./../ErrorMessages/Error.css";
 
 function Shift() {
-  const [Date, setDate] = useState(0);
-  const [TotalPicks, setTpicks] = useState(0);
-  const [Loom, setLoom] = useState(0);
+	const [Date, setDate] = useState(0);
+	const [TotalPicks, setTpicks] = useState(0);
+	const [Loom, setLoom] = useState(0);
 
-  const submitShiftData = () => {
-    Axios.post("http://localhost:3006/shiftInsert", {
-      Date: Date,
-      TotalPicks: TotalPicks,
-      Loom: Loom,
-    }).then(() => {
-      alert("successful insert");
-    });
-  };
+	const submitShiftData = () => {
+		Axios.post("http://localhost:3006/shiftInsert", {
+			Date: Date,
+			TotalPicks: TotalPicks,
+			Loom: Loom,
+		}).then(() => {
+			alert("successful insert");
+		});
+	};
 
-  return (
-    <div>
-      <div class="login">
-        <div class="form">
-          <div className="upperButtons">
-            <NavLink to="/worker1">
-              <button class="button">Back</button>
-            </NavLink>
-            <a class="button" href="#popup1">
-              Error
-            </a>
-          </div>
-          <h2>Shift Form</h2>
+	return (
+		<div>
+			<div class="login">
+				<div class="form">
+					<div className="upperButtons">
+						<NavLink to="/service2">
+							<button class="button">Back</button>
+						</NavLink>
+						<button class="button">Complete</button>
+						<a class="button" href="#popup1">
+							Error
+						</a>
+					</div>
+					<h2>Shift Form</h2>
 
-          <input
-            type="date"
-            placeholder="Date"
-            required
-            onChange={(e) => {
-              setDate(e.target.value);
-            }}
-          />
-          {/* <input type="number" placeholder="Order NO" required /> */}
-          <input
-            type="number"
-            placeholder="Loom"
-            required
-            onChange={(e) => {
-              setLoom(e.target.value);
-            }}
-          />
-          {/* <input type="number" placeholder="Shift" required /> */}
-          <input
-            type="number"
-            placeholder="Total Picks"
-            required
-            onChange={(e) => {
-              setTpicks(e.target.value);
-            }}
-          />
-          {/* <input type="number" placeholder="Meter" required /> */}
-          <input
-            type="submit"
-            value="Sign In"
-            class="submit"
-            onClick={submitShiftData}
-          />
-        </div>
-      </div>
-      <div id="popup1" class="overlay">
-        <div class="popup">
-          <h2>Report Error</h2>
-          <a class="close" href="#">
-            &times;
-          </a>
-          <div class="content">
-            <input type="text" placeholder="error" />
-            <button className="MainButton">Submit</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<input
+						type="date"
+						placeholder="Date"
+						required
+						onChange={(e) => {
+							setDate(e.target.value);
+						}}
+					/>
+					{/* <input type="number" placeholder="Order NO" required /> */}
+					<input
+						type="number"
+						placeholder="Loom"
+						required
+						onChange={(e) => {
+							setLoom(e.target.value);
+						}}
+					/>
+					{/* <input type="number" placeholder="Shift" required /> */}
+					<input
+						type="number"
+						placeholder="Total Picks"
+						required
+						onChange={(e) => {
+							setTpicks(e.target.value);
+						}}
+					/>
+					{/* <input type="number" placeholder="Meter" required /> */}
+					<input
+						type="submit"
+						value="Sign In"
+						class="submit"
+						onClick={submitShiftData}
+					/>
+				</div>
+			</div>
+			<div id="popup1" class="overlay">
+				<div class="popup">
+					<h2>Report Error</h2>
+					<a class="close" href="#">
+						&times;
+					</a>
+					<div class="content">
+						<input type="text" placeholder="error" />
+						<button className="MainButton">Submit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Shift;
